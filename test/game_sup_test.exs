deb931f434956ad3915fcf4f5bf1a88517adde8b
Joe-noh <goflb.jh@gmail.com>
2015-11-28 16:28:12 +0900

    test that spawned Game registers own uuid

--- test/game_sup_test.exs
+++ test/game_sup_test.exs
@@ -2,6 +2,7 @@ defmodule GameSupTest do
   use ExUnit.Case
 
   alias Reversi.GameSup
+  alias Reversi.NameResolver
 
   test "start_game returns uuid" do
     uuid1 = GameSup.start_game
@@ -9,5 +10,11 @@ defmodule GameSupTest do
 
     assert uuid1 != uuid2
     assert uuid1 |> is_binary
+  end
+
+  test "start_game registers uuid on NameResolver" do
+    uuid = GameSup.start_game
+
+    refute NameResolver.whereis(uuid) |> is_nil
   end
 end
