a31cc89e9055d2949b328769fba8a28b7dd458d1
Joe-noh <goflb.jh@gmail.com>
2015-11-30 18:17:24 +0900

    implement current_color/1

--- test/game_test.exs
+++ test/game_test.exs
@@ -16,4 +16,12 @@ defmodule GameTest do
     assert Game.put(game_id, "c", "5", :black) == :ok
     assert {:error, _} = Game.put(game_id, "d", "6", :black)
   end
+
+  test "current_color returns the color of disk which is going to be put", %{game_id: game_id} do
+    assert Game.current_color(game_id) == :black
+
+    Game.put(game_id, "c", "5", :black)
+
+    assert Game.current_color(game_id) == :white
+  end
 end
